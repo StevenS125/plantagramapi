4757cdf2a97a8a505e0e2438e2cbc2f1de0ae883
Steven Snyder <sts12589@gmail.com>
2020-09-30 13:10:48 -0400

    first commit

---
+++ src/main/java/com/plantagram/plantagramapi/PlantagramapiApplication.java
@@ -0,0 +1,13 @@
+package com.plantagram.plantagramapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantagramapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantagramapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantagram/plantagramapi/PlantagramapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantagram.plantagramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantagramapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
